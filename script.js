d8c4150d15df53798c89ea9ef91801b7a67f1781
Andy Bartkiv <Andy.Bartkiv@gmail.com>
2020-04-23 22:17:15 +0300

    rotation and borders bump added

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function createMatrix(w, h) {
 function draw() {
   context.fillStyle = "#001";
   context.fillRect(0,0, canvas.width, canvas.height);
+  drawMatrix(arena, {x:0, y:0});
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -60,7 +61,28 @@ function merge(arean, player) {
 
 function playerDrop() {
     player.pos.y++;
+    if (collide(arena, player)) {
+      player.pos.y--;
+      merge(arena, player);
+      player.pos.y = 0;
+    }
     dropCounter = 0;  
+}
+
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
+  }
+}
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y){
+    for (let x = 0; x < y; ++x) {
+      [matrix[x][y], matrix[y][x]] =
+        [matrix[y][x], matrix[x][y]];
+    }
+  }
 }
 
 let dropCounter = 0;
@@ -94,9 +116,9 @@ const player = {
 
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
-    player.pos.x--;
+    playerMove(-1);
   } else if (event.keyCode == 39) {
-    player.pos.x++;
+    playerMove(+1);
   } else if (event.keyCode == 40) {
     playerDrop();
   }
