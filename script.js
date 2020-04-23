434780e0d173ec7736c2ab47908a8cd954aecfd4
Andy Bartkiv <Andy.Bartkiv@gmail.com>
2020-04-23 22:17:52 +0300

    rotation and border bump added

--- script.js
+++ script.js
@@ -76,12 +76,32 @@ function playerMove(dir) {
   }
 }
 
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].lenght) {
+      rotate(player.matrix, -dir);
+      player.pos = pos;
+      return;
+    }
+  }
+}
+
 function rotate(matrix, dir) {
   for (let y = 0; y < matrix.length; ++y){
     for (let x = 0; x < y; ++x) {
       [matrix[x][y], matrix[y][x]] =
         [matrix[y][x], matrix[x][y]];
     }
+  }
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
   }
 }
 
@@ -121,6 +141,8 @@ document.addEventListener('keydown', event => {
     playerMove(+1);
   } else if (event.keyCode == 40) {
     playerDrop();
+  } else if (event.keyCode == 38) {
+    playerRotate(+1);
   }
 }); 
 
